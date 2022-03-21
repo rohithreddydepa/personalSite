b89ea9e5da25abd053b56555b7e6f5a25b9895db
rohithdepa <rohithreddydepa>
2022-03-21 22:29:37 +0530

    removed the pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { summayPipe } from 'src/summay.pipe';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { VideoComponent } from './video/video.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, summayPipe, VideoComponent],
+  declarations: [AppComponent, HeaderComponent, VideoComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
