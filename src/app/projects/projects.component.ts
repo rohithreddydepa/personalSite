47dd8e18f6ef7eca4d57d8718c8164c599747628
rohithdepa <rohithreddydepa>
2022-11-06 21:06:22 +0530

    added image and projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -14,33 +14,32 @@ export class ProjectsComponent implements OnInit {
   constructor() {
     this.events1 = [
       {
+        status: 'Tag detection',
+        icon: PrimeIcons.CIRCLE,
+        readMore: false,
+        link: 'https://major13.herokuapp.com',
+        data: 'Tagging is the process of identifying the label or class to which the piece of information belongs to. Tagging helps in easy retrieval and organization of data. We present a project that performs tagging for various category of questions using Machine Learning and Natural Language Processing.',
+      },
+      {
         status: "Android App'S",
         icon: PrimeIcons.CIRCLE,
         readMore: false,
-        link: '',
+        link: 'https://github.com/rohithreddydepa/Andriodapps',
         data: 'I have build some Android namely Brain trainer, Tic-tac toe, Location copier, Currency converter, Clone text .In brain trainer app user have to answer the the math questions as many as he can in 30s for each question and time gradually decreases for next question, Currency converter is an basic app which is used to convert the American dollars to Indian rupees.You can know the functionality of the other apps by visiting the github',
       },
       {
         status: 'Full Stack',
         icon: PrimeIcons.CIRCLE,
         readMore: false,
-        link: '',
+        link: 'https://github.com/rohithreddydepa/personalSite',
         data: "Every Business starting from startup's to multi national companies they need a website to show case there products or services to the customer so bulding website should be very essential . As a full stack Web developer i can build a website from scratch and and deploye it on servers like aws or custom server using nodejs and mongodb.",
       },
       {
-        status: 'Shipped',
+        status: 'Audio Book',
         icon: PrimeIcons.CIRCLE,
         readMore: false,
-        link: '',
-        data: '',
-      },
-
-      {
-        status: 'Delivered',
-        icon: PrimeIcons.CIRCLE,
-        readMore: false,
-        link: '',
-        data: '',
+        link: 'https://github.com/rohithreddydepa/audiobook',
+        data: 'Reading a book is a very good habit but due to heavy work schedule it is impossible to read a book to solve the issue audiobooks are created ,so by Listening to the book when Exercising or traveling you can complete the book with ease So I have created a python program which can convert pdf books to mp3 files You can clone the Repository in my Github and convert your pdf books',
       },
     ];
 
